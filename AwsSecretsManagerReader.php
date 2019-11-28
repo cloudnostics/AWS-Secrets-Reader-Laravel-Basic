361c24469e402cab1bc914b45749cf96bb1df825
Dean LJ <d.laytonjames@gmail.com>
2019-11-28 23:25:50 +0000

    Initial commit of code

---
+++ AwsSecretsManagerReader.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * Copyright 2019 Quantic Ventures Ltd t/a CloudPeriscope.com. All Rights Reserved.
+ *
+ * This file is licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. A copy of
+ * the License is located at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Code based on the example at
+ * https://docs.aws.amazon.com/code-samples/latest/catalog/php-secretsmanager-GetSecretValue.php.html
+ */
+
+namespace App\Helpers;
+
+use Aws\Credentials\CredentialProvider;
+use Aws\Exception\AwsException;
+use Aws\SecretsManager\SecretsManagerClient;
+
+/**
+ * This code expects that you have AWS credentials set up per:
+ * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
+ *
+ */
+class AwsSecretsManagerReader {
+
+    public static $aws_client;
+
+    public static $version = '2017-10-17';
+
+    public static $secret_value;
+
+    /**
+     * The makeClient method relies on the aws default provider chain
+     * https://docs.aws.amazon.com/sdk-for-php/v3/developer-guide/guide_credentials.html
+     *
+     * If there are environment vars present it will use those
+     * If there is a shared credentials file it will use that
+     * If your EC2 instance/Lambda has a service role it will use that
+     *
+     * @param array $config
+     *
+     */
+    public static function makeClient(array $config = [])
+    {
+        try {
+            $credentials_provider = null;
+
+            // Use the default credential provider
+            // This checks the environment, credentials file an then instance roles
+            // see https://docs.aws.amazon.com/aws-sdk-php/v3/api/class-Aws.Credentials.CredentialProvider.html#_defaultProvider
+            // so you must set env vars, have a credentials file or be running on an ec2 instance with
+            // a role that gives access to your resources (secrets manager)
+
+            // check for profile name passed
+            $profile = $config['profile'] ?? null;
+            if ($profile) {
+                unset($config['profile']);
+            }
+
+            // check for specific creds passed
+            if (isset($config['credentials'])) {
+                $credentials_provider = $config['credentials'];
+            }
+
+            // todo test ec2 instance role
+
+            // todo test ecs role
+
+            if (!$credentials_provider) {
+                $credentials_provider = CredentialProvider::defaultProvider($config);
+            }
+
+            $client_options = [
+                'version' => self::$version,
+                'region' => $config['region'] ?? env('AWS_DEFAULT_REGION', 'eu-west-1'),
+            ];
+
+            // add the .credentials profile name in if we have it
+            if ($profile) {
+                $client_options['profile'] = $profile;
+            }
+
+            // if we have a 'truthy' credentials provider then add it in
+            if ($credentials_provider) {
+                $client_options['credentials'] = $credentials_provider;
+            }
+
+            // if credentials or profile not passed in $client_options then the sdk
+            // will look for an instance or ecs service role
+            self::$aws_client = new SecretsManagerClient($client_options);
+
+        } catch (AwsException $e) {
+            $error = $e->getAwsErrorCode();
+            // Handle the exception here, and/or rethrow as needed.
+            throw $e;
+        }
+    }
+
+    /**
+     * @param $secret_name
+     *
+     * @return false|mixed|string|null
+     */
+    public static function getSecret($secret_name)
+    {
+        try {
+            if (!self::$aws_client) {
+                self::makeClient();
+            }
+
+            $result = self::$aws_client->getSecretValue([
+                'SecretId' => $secret_name,
+            ]);
+
+        } catch (AwsException $e) {
+            $error = $e->getAwsErrorCode();
+
+            if ($error === 'DecryptionFailureException') {
+                // Secrets Manager can't decrypt the protected secret text using the provided AWS KMS key.
+                // Handle the exception here, and/or rethrow as needed.
+                throw $e;
+            }
+            if ($error === 'InternalServiceErrorException') {
+                // An error occurred on the server side.
+                // Handle the exception here, and/or rethrow as needed.
+                throw $e;
+            }
+            if ($error === 'InvalidParameterException') {
+                // You provided an invalid value for a parameter.
+                // Handle the exception here, and/or rethrow as needed.
+                throw $e;
+            }
+            if ($error === 'InvalidRequestException') {
+                // You provided a parameter value that is not valid for the current state of the resource.
+                // Handle the exception here, and/or rethrow as needed.
+                throw $e;
+            }
+            if ($error === 'ResourceNotFoundException') {
+                // We can't find the resource that you asked for.
+                // Handle the exception here, and/or rethrow as needed.
+                throw $e;
+            }
+        }
+
+        return self::decodeValue($result);
+    }
+
+    /**
+     * decode the AWS secrets manager secret as required
+     *
+     * @param $secret
+     *
+     * @return false|mixed|string|null
+     */
+    public static function decodeValue($secret)
+    {
+        // Decrypts secret using the associated KMS CMK.
+        // Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if (isset($secret['SecretString'])) {
+            return $secret['SecretString'];
+        }
+
+        if (\is_array($secret) && isset($secret['SecretBinary'])) {
+            return base64_decode($secret['SecretBinary']);
+        }
+
+        return null;
+    }
+
+    /**
+     * Get a specific key value from a secrets manager secret
+     *
+     * @param      $secret_name
+     * @param null $key
+     *
+     * @return false|mixed|string|null
+     * @throws \Exception
+     */
+    public static function getSecretKey($secret_name, $key = null)
+    {
+        // get the secret from AWS secrets manager
+        self::$secret_value = json_decode(self::getSecret($secret_name), true);
+
+        // no key provided so we are expecting a single value, return everything we have
+        if (!$key) {
+            return self::$secret_value;
+        }
+
+        // if we have a key then we are expecting key/value pairs as a JSON string
+        if (!isset(self::$secret_value[$key])) {
+            throw new \Exception('[CloudPeriscope-AwsSecretsManagerReader:WARN] Secret Key does not exist');
+        }
+
+        return self::$secret_value[$key];
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # AWS-Secrets-Reader-Laravel-Basic
 A Basic Laravel helper to read AWS Secrets Manager Secrets
+
+Once you setup your database credentials to auto rotate using AWS secrets manager then you'll need your app to pull the db connection info synamically from AWS Secrets Manager.
+
+Your Laravel app should have a directory called app/Helpers where the file AwsSecretsManagerReader.php should go.
+
+Your composer update/dump-autoload should pick it up.
+
+To setup a DB connection to use your secret, create a new connection in config/database.php something like:
+
+
+        'mysql_rds' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => AwsSecretsManagerReader::getSecretKey('mysql', 'host'),
+            'port' => AwsSecretsManagerReader::getSecretKey('mysql', 'port'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' =>  AwsSecretsManagerReader::getSecretKey('mysql', 'username'),
+            'password' => AwsSecretsManagerReader::getSecretKey('mysql', 'password'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+If you cache your Laravel config then you may need to do something like the following (untested at the moment)
+
+
+        'mysql_rds' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => function () {
+                            return AwsSecretsManagerReader::getSecretKey('mysql', 'host');
+                    },
+            'port' => function () {
+                            return AwsSecretsManagerReader::getSecretKey('mysql', 'port');
+                    },
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' =>  function () {
+                            return AwsSecretsManagerReader::getSecretKey('mysql', 'username');
+                    },
+            'password' =>  function () {
+                            return AwsSecretsManagerReader::getSecretKey('mysql', 'password');
+                    },
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+
+
